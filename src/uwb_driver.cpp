38f7612eea409ee924070e8c2cbf5a4e75c8bfe5
britsknguyen <thienminh.npn@gmail.com>
2016-11-04 09:20:26 +0000

    range info tested, data info haven't

--- src/uwb_driver.cpp
+++ src/uwb_driver.cpp
@@ -555,7 +555,7 @@ int main(int argc, char *argv[])
 
                 uwb_range_publisher.publish(uwb_range_info_msg);
 
-                printf("Received distance message:Time=%f\tIndex=%d\tID=%d\td=%f, de = %f, dd = %f, dde = %f\tsw=%d\ttu = %zu\tant=%d\tx=%.3f\ty=%.3f\tz=%.3f\n",
+                printf("RANGEINFO:Time=%.4f\tIndex=%d\tID=%d\td=%6.3f, de = %6.3f, dd = %6.3f, dde = %6.3f\tsw=%d\ttu = %zu\tant=%d\tx=%6.2f\ty=%6.2f\tz=%6.2f\n",
                        uwb_range_info_msg.stamp.toSec(),
                        uwb_range_info_msg.responder_idx+1,
                        uwb_range_info_msg.responder_id,
@@ -569,7 +569,6 @@ int main(int argc, char *argv[])
                        uwb_range_info_msg.responder_location.x,
                        uwb_range_info_msg.responder_location.y,
                        uwb_range_info_msg.responder_location.z);
-                printf("___________\n");
 
             }
             else if(msg_type == DATAINFO)
@@ -587,16 +586,16 @@ int main(int argc, char *argv[])
 
                 uwb_data_publisher.publish(uwb_data_info_msg);
 
-                printf("Received data message: Time=%f\tIndex:%d\tID:%d\ttu = %zu\tant=%d\trqstr loc: %.3f\t%.3f\t%.3f\n",
+                printf("DATAINFO: Time=%.4f\tIndex:%d\tID:%d\ttu = %zu\tant=%d\tData bytes: %d\t{",
                        uwb_data_info_msg.stamp.toSec(),
                        uwb_data_info_msg.source_idx + 1,
                        uwb_data_info_msg.source_id,
                        uwb_data_info_msg.uwb_time,
-                       uwb_data_info_msg.antenna);
-                printf("Received %d bytes: ", dataInfo.dataSize);
+                       uwb_data_info_msg.antenna,
+                       dataInfo.dataSize);
                 for(int i = 0; i < dataInfo.dataSize; i++)
                     printf("%02x ", dataInfo.data[i]);
-                printf("\n");
+                printf("}\n");
 
 
             }
@@ -605,4 +604,5 @@ int main(int argc, char *argv[])
         if(restEnable)
             sleepTime.sleep();
     }
+    rcmIfClose();
 }
