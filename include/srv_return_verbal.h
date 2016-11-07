80b321b75a5a0363e6e65121a49a459b2cf0896b
britsknguyen <thienminh.npn@gmail.com>
2016-11-07 21:35:03 +0800

    forgot to add include and srv folder

---
+++ include/srv_return_verbal.h
@@ -0,0 +1,36 @@
+//Verbal definitions for the mode commands service
+#ifndef P4XX_CONFIRMED
+#define P4XX_CONFIRMED              0
+#endif
+
+#ifndef P4XX_CMD_SENT
+#define P4XX_CMD_SENT               1
+#endif
+
+#ifndef P4XX_CONFIRM_TIMEOUT
+#define P4XX_CONFIRM_TIMEOUT        2
+#endif
+
+
+//Verbal definitions for the range-comm service
+#ifndef RANGE_RQST
+#define RANGE_RQST                  0
+#endif
+
+#ifndef SEND_DATA
+#define SEND_DATA                   1
+#endif
+
+#ifndef RANGE_RQST_DATA
+#define RANGE_RQST_DATA             2
+#endif
+
+#ifndef SET_RQST_DATA
+#define SET_RQST_DATA               3
+#endif
+
+#ifndef SET_RSPD_DATA
+#define SET_RSPD_DATA               4
+#endif
+
+
