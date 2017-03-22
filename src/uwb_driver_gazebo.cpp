fd4894d21e17eb0611517503eae90b4f99baccfc
Jeffsan <wang.chen@zoho.com>
2017-03-22 17:46:38 +0800

    add uwb simulation for double antena

--- src/uwb_driver_gazebo.cpp
+++ src/uwb_driver_gazebo.cpp
@@ -1,7 +1,12 @@
 #include "ros/ros.h"
 #include "uwb_driver/UwbRange.h"
 #include "geometry_msgs/PoseStamped.h"
+#include <Eigen/Dense>
+#include "eigen_conversions/eigen_msg.h"
 #include <sstream>
+
+using namespace Eigen;
+using namespace tf;
 ros::Publisher uwb_pub;
 std::vector<int> nodesId;
 std::vector<double> nodesPos;
@@ -10,14 +15,10 @@ int count = 0;
 std::random_device rd;
 std::mt19937 gen(rd());
 std::normal_distribution<> d(0,0.02);
+Eigen::Affine3d offset_left, offset_right;
 
 void gazeboCallback(const geometry_msgs::PoseStamped pose)
 {
-    double distance = sqrt(
-        pow(pose.pose.position.x - nodesPos[count*3  ], 2)+
-        pow(pose.pose.position.y - nodesPos[count*3+1], 2)+
-        pow(pose.pose.position.z - nodesPos[count*3+2], 2));
-
     uwb_driver::UwbRange msg;
     msg.header = std_msgs::Header();
     msg.header.frame_id = "uwb";
@@ -26,6 +27,28 @@ void gazeboCallback(const geometry_msgs::PoseStamped pose)
     msg.requester_idx =  nodesId.back()%100;
     msg.responder_id  = nodesId[count];
     msg.responder_idx = nodesId[count]%100;
+
+    Eigen::Affine3d pose_eigen;
+    poseMsgToEigen(pose.pose, pose_eigen);
+    if (count%2)
+    {
+        msg.antenna = 0;
+        pose_eigen = pose_eigen * offset_left;
+    }
+    else
+    {
+        msg.antenna = 255;
+        pose_eigen = pose_eigen * offset_right;
+    }
+
+
+
+    double distance = sqrt(
+        pow(pose_eigen(0,3) - nodesPos[count*3  ], 2)+
+        pow(pose_eigen(1,3) - nodesPos[count*3+1], 2)+
+        pow(pose_eigen(2,3) - nodesPos[count*3+2], 2));
+
+
     msg.distance = distance + d(gen);
     msg.distance_err = 0.02;
     msg.responder_location.x = nodesPos[count*3];
@@ -58,6 +81,11 @@ int main(int argc, char **argv)
     node_num = nodesId.size() - 1;
     
     ROS_WARN("Publishing..");
+
+    offset_left.setIdentity();
+    offset_right.setIdentity();
+    offset_left.translate(Vector3d(-0.2, 0, 0));
+    offset_right.translate(Vector3d(0, 0.2, 0));
 
     ros::spin();
 
