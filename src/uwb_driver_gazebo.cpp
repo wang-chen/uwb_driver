78ebcb0b79c0f4f13e97bef54380797b453a409d
Jeffsan <wang.chen@zoho.com>
2017-03-23 17:50:01 +0800

    add simulation for two antennas and pubslish anchors tf

--- src/uwb_driver_gazebo.cpp
+++ src/uwb_driver_gazebo.cpp
@@ -4,8 +4,11 @@
 #include <Eigen/Dense>
 #include "eigen_conversions/eigen_msg.h"
 #include <sstream>
-
+#include <string.h>
+#include <tf/transform_broadcaster.h>
+#include <tf_conversions/tf_eigen.h>
 using namespace Eigen;
+// using namespace std;
 using namespace tf;
 ros::Publisher uwb_pub;
 std::vector<int> nodesId;
@@ -31,24 +34,32 @@ void gazeboCallback(const geometry_msgs::PoseStamped pose)
 
     Eigen::Affine3d pose_eigen;
     poseMsgToEigen(pose.pose, pose_eigen);
+    static tf::TransformBroadcaster br;
+    tf::Transform transform;
+
     if (count_antenna%2)
     {
-        msg.antenna = 0;
+        msg.antenna = 1;
         pose_eigen = pose_eigen * offset_left;
-        count = (count+1)%node_num;
+        tf::transformEigenToTF (pose_eigen, transform);
+        br.sendTransform(tf::StampedTransform(transform, pose.header.stamp, pose.header.frame_id, "offset_left"));
     }
     else
     {
-        msg.antenna = 255;
+        msg.antenna = 2;
         pose_eigen = pose_eigen * offset_right;
+        tf::transformEigenToTF (pose_eigen, transform);
+        br.sendTransform(tf::StampedTransform(transform, pose.header.stamp, pose.header.frame_id, "offset_right"));
     }
     count_antenna = count_antenna+1;
-
 
     double distance = sqrt(
         pow(pose_eigen(0,3) - nodesPos[count*3  ], 2)+
         pow(pose_eigen(1,3) - nodesPos[count*3+1], 2)+
         pow(pose_eigen(2,3) - nodesPos[count*3+2], 2));
+
+    transform.setOrigin( tf::Vector3(nodesPos[count*3  ],nodesPos[count*3+1], nodesPos[count*3+2]));
+    br.sendTransform(tf::StampedTransform(transform, pose.header.stamp, pose.header.frame_id, std::to_string(count)+"anchor"));
 
 
     msg.distance = distance + d(gen);
@@ -57,9 +68,11 @@ void gazeboCallback(const geometry_msgs::PoseStamped pose)
     msg.responder_location.y = nodesPos[count*3+1];
     msg.responder_location.z = nodesPos[count*3+2];
     uwb_pub.publish(msg);
-    
 
-    ros::Duration(0.0666).sleep();
+    if (count_antenna%2)
+        count = (count+1)%node_num;
+
+    ros::Duration(0.025).sleep();
 }
 
 int main(int argc, char **argv)
@@ -76,7 +89,7 @@ int main(int argc, char **argv)
         for(auto it:nodesPos)
             ROS_WARN("Get node position: %4.2f", it);
 
-    ros::Subscriber sub = n.subscribe("/ground_truth/pose", 1, gazeboCallback);
+    ros::Subscriber sub = n.subscribe("/ground_truth_to_tf/pose", 1, gazeboCallback);
 
     uwb_pub = n.advertise<uwb_driver::UwbRange>("/uwb_endorange_info", 0);
     
@@ -86,8 +99,9 @@ int main(int argc, char **argv)
 
     offset_left.setIdentity();
     offset_right.setIdentity();
-    offset_left.translate(Vector3d(-0.2, 0, 0));
-    offset_right.translate(Vector3d(0, 0.2, 0));
+    offset_left.translate(Vector3d(-1, 0, 0));
+    offset_right.translate(Vector3d(1, 0, 0));
+    // cout<<offset_right<<" "<<offset_left<<endl;
 
     ros::spin();
 
